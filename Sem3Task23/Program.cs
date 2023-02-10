457058882ab3166c5831bddc1a47c5d77a5f4415
Paul <bartenevp@mail.ru>
2023-02-10 19:27:30 +0300

    Homework 10.02.2023

---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на 
+//вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// Вводим число
+int number = ReadInt("Введите число N: ");
+// Делаем цикл от 1 до нашего числа
+for (int i = 1; i <= number; i++)
+{ 
+    // Выводим куб числа от 1 до нашего числа
+    Console.Write($"{i*i*i} ");
+}
+
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
