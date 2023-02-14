b8a91b433786c7130764e751fe8f55c2f5513dc4
Paul <bartenevp@mail.ru>
2023-02-14 12:40:57 +0300

    HomeWork seminar 4 completed

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine()??"0");
+
+int result = (int)Math.Pow(num1, num2);
+
+Console.WriteLine($"{num1} в степени {num2} = {result}");
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()??"0");
+
+int len = NumberLen(num);
+SumNumbers(num, len);
+
+
+// Функция ввода
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
